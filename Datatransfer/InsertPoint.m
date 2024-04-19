403c2dccbb371ea272b7a641ebf888ab4139f91c
danaaa1222 <164391436+danaaa1222@users.noreply.github.com>
2024-04-19 22:41:28 +0800

    Delete Datatransfer/InsertPoint.m

--- Datatransfer/InsertPoint.m
+++
@@ -1,8 +0,0 @@
-function [Waveform_back] = InsertPoint( Waveform,insertpoint )
-
-x = 1:1:length(Waveform); 
-% insertpoint=0;
-v =double( Waveform);
- xq=1:(1/(insertpoint+1)):length(Waveform);
-Waveform_back= interp1(x,v,xq);
-end
